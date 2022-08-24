a3f08cd48b3da05970b32ed0bc7f19989e7be4fe
Nantus Groenwald <91613376+NantusGroenewald@users.noreply.github.com>
2022-08-24 13:09:15 +0200

    Spring Boot Added

---
+++ loginportal/src/test/java/com/nantus/loginportal/LoginportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nantus.loginportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
